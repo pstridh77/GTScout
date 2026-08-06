6c6352ec0b50468e3db6f4fa87cc667b43de0107
Patrik Stridh <pstridh77@gmail.com>
2026-08-06 23:05:17 +0200

    Kategori tillaggd

--- js/app.js
+++ js/app.js
@@ -2,16 +2,19 @@ const marken = [
     {
         namn: "Värme",
         grupp: "Familjescout",
+        kategori: "Eld",
         bild: "images/marken/varme.png"
     },
     {
         namn: "Tända",
         grupp: "Spårare",
+        kategori: "Eld",
         bild: "images/marken/tanda.png"
     },
     {
         namn: "Brinna",
         grupp: "Upptäckare",
+        kategori: "Eld",
         bild: "images/marken/brinna.png"
     }
 ];
@@ -19,6 +22,7 @@ const marken = [
 const grid = document.getElementById("badgeGrid");
 
 marken.forEach(marke => {
+
     const card = document.createElement("div");
 
     card.className = "badge";
@@ -28,9 +32,32 @@ marken.forEach(marke => {
         <h3>${marke.namn}</h3>
         <p>${marke.grupp}</p>
     `;
+
+    
+
     card.addEventListener("click", () => {
-    alert(marke.namn);
+
+        document.getElementById("details").innerHTML = `
+            <img
+                src="${marke.bild}"
+                alt="${marke.namn}"
+                class="detail-image">
+
+            <div class="detail-text">
+                <h2>${marke.namn}</h2>
+                <p>
+                    <strong>Målgrupp:</strong>
+                    ${marke.grupp}
+                </p>
+                <p>
+                    <strong>Kategori:</strong>
+                    ${marke.kategori}
+                </p>
+            </div>
+        `;
+
     });
 
     grid.appendChild(card);
+
 });
